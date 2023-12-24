7aa6c28fd1bea9ca61d3cc77890d9fbdca117e97
Cleidison Silva <cleidison85@gmail.com>
2023-12-24 01:18:37 -0300

    Desafio DIO Concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Testando a classe Nokia
+            Nokia nokia = new Nokia("123456", "NokiaModelo", "IMEI123", 64);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            // Testando a classe Iphone
+            Iphone iphone = new Iphone("789012", "iPhoneModelo", "IMEI456", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Twitter");
+        }
+    }
+}
